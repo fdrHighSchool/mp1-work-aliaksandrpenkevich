b977f5a976489d4d5ce9d6eab99212e311004b94
AP Java Period 5 <apjavaperiod5@AC1MWK13UJC9H.local>
2022-10-04 11:49:59 -0400

    1

---
+++ 01-favorite-movie/Main (1).java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println(" My favorite series is the Policeman from Rublyovka.");
+    System.out.print("This is a black humor comedy series that makes fun of police corruption in high-end neighborhoods.This is a black humor comedy series that makes fun of police corruption in high-end neighborhoods.");
+    System.out.print("Rublyovka is a very rich suburban area in Moscow, where many stars and businessmen live.");
+    System.out.println("The main Hero is Vladimir, a man who has a 'demon' - in his opinion.");
+    System.out.print("Demon - Grigory, a policeman below his rank, whose parents were killed a long time ago and he decided to become a policeman because of this, by the way, according to the plot, he was a brilliant worker; they left a multimillion-dollar inheritance (parents), because of which Vladimir constantly disliked Gregory, and they constantly made fun of each other.");
+  }
+}
---
+++ 02-rocketship/Main.java
@@ -0,0 +1,31 @@
+//  Name: Aliaksandr and Yuxin
+//  Period: 5
+//  Date: 09/23/2022
+
+ 
+
+class Main {
+  public static void main(String[] args) {
+    triangle();
+    square();
+    usa();
+    square();
+    triangle();
+  }
+  public static void triangle() {
+    System.out.println("   /\\ "); 
+    System.out.println("  /  \\");
+    System.out.println(" /    \\");
+      }
+  public static void square() {
+    System.out.println("+------+"); 
+    System.out.println("|      |");
+    System.out.println("|      |");
+    System.out.println("+------+");
+      }
+      public static void usa() {
+    System.out.println("|United|"); 
+    System.out.println("|States|");
+      }
+  
+}
