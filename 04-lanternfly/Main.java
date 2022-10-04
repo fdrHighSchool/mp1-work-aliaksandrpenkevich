e5309500c2579b249cd0070ce65371ec3ca10bce
AP Java Period 5 <apjavaperiod5@AC1MWK13UJC9H.local>
2022-10-04 12:01:44 -0400

    1

---
+++ 04-lanternfly/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("How many pull ups you can do for 1 set?");
+    System.out.println("*answer honestly*");
+
+    System.out.print("Number of pull ups? ");
+    int num = input.nextInt();
+    
+    if (num<10){
+      System.out.println("I'll trust");
+    } //end if statment
+    else if {
+      System.out.print("Honestly !? ");
+    num = input.nextInt();
+    input.close ();
+      
+    } // end else statement
+  } // end main method
+} // end of class 
